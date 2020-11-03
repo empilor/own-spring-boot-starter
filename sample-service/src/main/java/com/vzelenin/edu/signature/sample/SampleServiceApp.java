7e077343010f569e3bdee9057e70ccfc252cfcb7
Vadim_Zelenin <empilor@gmail.com>
2020-11-03 14:25:37 +0300

    commit an own starter (signature-starter) with a rest controller; commit sample-service that uses an own starter

---
+++ sample-service/src/main/java/com/vzelenin/edu/signature/sample/SampleServiceApp.java
@@ -0,0 +1,10 @@
+package com.vzelenin.edu.signature.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleServiceApp.class, args); }
+}
---
+++ signature-starter/src/main/java/com/vzelenin/edu/signature/starter/autoconfigure/SignatureAutoConfiguration.java
@@ -0,0 +1,7 @@
+package com.vzelenin.edu.signature.starter.autoconfigure;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.vzelenin.edu.signature.starter")
+public class SignatureAutoConfiguration {
+}
