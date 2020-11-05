59977980c1bc9b9fc487f32fa50e5f85e8aac2dd
Vadim_Zelenin <empilor@gmail.com>
2020-11-05 14:10:53 +0300

    added username and retention, properties file, conditional extension enabling

--- signature-starter/src/main/java/com/vzelenin/edu/signature/starter/autoconfigure/SignatureAutoConfiguration.java
+++ signature-starter/src/main/java/com/vzelenin/edu/signature/starter/autoconfigure/SignatureAutoConfiguration.java
@@ -1,7 +1,10 @@
 package com.vzelenin.edu.signature.starter.autoconfigure;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.vzelenin.edu.signature.starter")
+//This annotation turns off own starter if property has value "false". Default "true"
+@ConditionalOnProperty(value = "demo.signature.enabled", havingValue = "true", matchIfMissing = true)
 public class SignatureAutoConfiguration {
 }
--- signature-starter/src/main/java/com/vzelenin/edu/signature/starter/controller/SignatureController.java
+++ signature-starter/src/main/java/com/vzelenin/edu/signature/starter/controller/SignatureController.java
@@ -1,13 +1,29 @@
 package com.vzelenin.edu.signature.starter.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @RestController
 public class SignatureController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SignatureController.class);
+    private final String username;
+    private final Duration retentionPeriod;
+
+    public SignatureController(@Value("${demo.signature.username:NONAME}") String username,
+                               @Value("${demo.signature.extra.retention-period:5m}") Duration retentionPeriod) {
+        this.username = username;
+        this.retentionPeriod = retentionPeriod;
+        LOGGER.info("Started with 'username' - {}, 'retention period' - {}", username, retentionPeriod);
+    }
+
     @GetMapping("/api/v1/signature")
     public String getSignature() {
-        return "Best Regards";
+        return "Best Regards,\n" + username + "\nEmail will be deleted after " + retentionPeriod;
     }
 }
